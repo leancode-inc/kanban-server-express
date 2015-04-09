d3120eedf9587a360c1bda3f52d8d392c5ab5d44
Sai Gudigundla <saip106@msn.com>
2015-04-09 12:22:51 -0400

    got basic post working

--- routes/user-stories.js
+++ routes/user-stories.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 
-var userStories = [
-    {id: '1', title: 'foo', description: 'This is a foo user story'},
-    {id: '2', title: 'bar', description: 'This is a bar user story'},
-    {id: '3', title: 'baz', description: 'This is a baz user story'},
-];
+var userStories = [];
 
 router.get('/', function (request, response, next) {
     response.send(userStories);
@@ -19,8 +15,10 @@ router.get('/:id', function (request, response, next) {
 });
 
 router.post('/', function (request, response, next) {
+    console.log('creating new user story');
     if (request.body) {
-        userStories.add(request.body);
+        userStories.push(request.body);
+        response.send(200);
     }
 });
 
